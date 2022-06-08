d3a0e4889f15179d7966943e467f4cb091ea03e1
Lorenzo Catalano <lorenzo@pop-os.localdomain>
2022-06-08 18:20:28 +0200

    Fuck me 3

--- frontend/mix.exs
+++ frontend/mix.exs
@@ -46,7 +46,7 @@ defmodule Frontend.MixProject do
       {:gettext, "~> 0.18"},
       {:jason, "~> 1.2"},
       {:plug_cowboy, "~> 2.5"},
-      {:casino, path: "../casino"}
+      {:casino, path: "../backend"}
     ]
   end
 
